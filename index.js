b4988cd12363bcaef3e79e544e3256dcf0b617a1
April <april.koh@yale.edu>
2016-01-15 17:36:23 -0500

    tweaked listening command

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var config = require('./config/config.js');
 var app = express();
 
 var host = process.env.IP || 'localhost';
-var port = process.env.IP || 4000;
+var port = process.env.PORT || 4000;
 var sessionSecret = process.env.SESSION_SECRET || 'e70a1e1ee4b8f662f78'
 
 app.use('/static', express.static(__dirname + '/static'));
