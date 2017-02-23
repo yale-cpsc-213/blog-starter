e95b781c1aaa27ac977b040f3d9498e5b3108ac3
Kyle L. Jensen <kljensen@gmail.com>
2017-02-23 14:44:24 -0500

    Change localhost to 0.0.0.0

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var express = require('express');
 var config = require('./config/config.js');
 var app = express();
 
-var host = process.env.IP || 'localhost';
+var host = process.env.IP || '0.0.0.0';
 var port = process.env.PORT || 4000;
 var sessionSecret = process.env.SESSION_SECRET || 'e70a1e1ee4b8f662f78';
 
 app.use('/static', express.static(__dirname + '/static'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 config(app, host, port, sessionSecret);
 
 var server = app.listen(port, host, function () {
